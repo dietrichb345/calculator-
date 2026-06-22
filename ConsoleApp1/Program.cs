53f8ef55844bb349030a6a25cf91bacad729039e
dietrichb345 <boettgerdietrich@icloud.com>
2026-06-22 12:18:29 -0500

    added multiplication

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("enter a number");
+string addition = Console.ReadLine();
+Console.WriteLine(addition);
+int square =2;
+int parsedaddition=int.Parse(addition);
+int result=square*parsedaddition;
+Console.WriteLine(result);
